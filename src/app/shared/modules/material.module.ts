42ea30da7ccc2e7e7e29db51c67b463dfcba853f
dymafr <erwan1309@hotmail.com>
2021-01-11 12:36:24 +0100

    1 file modified

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatFormField } from "@angular/material/form-field";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 
-const MATERIALS = [MatFormField, MatInputModule, MatCheckboxModule];
+const MATERIALS = [MatFormFieldModule, MatInputModule, MatCheckboxModule];
 
 @NgModule({
   imports: MATERIALS,
