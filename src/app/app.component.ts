1a5a26dcff4086f815593aeb5added265f3d4844
dymafr <erwan1309@hotmail.com>
2021-01-11 12:33:43 +0100

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup } from "@angular/forms";
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   public form: FormGroup;
