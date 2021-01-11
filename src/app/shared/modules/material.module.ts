7cdeaec740fc77e5946b81b28cd9b66ec14de3e7
dymafr <erwan1309@hotmail.com>
2021-01-11 12:40:30 +0100

    2 files modified

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from "@angular/core";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 
-const MATERIALS = [MatFormFieldModule, MatInputModule, MatCheckboxModule];
+const MATERIALS = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatSlideToggleModule
+];
 
 @NgModule({
   imports: MATERIALS,
