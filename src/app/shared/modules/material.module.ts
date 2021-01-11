d0a89838a05451803aabef92d3e122d288530a10
dymafr <erwan1309@hotmail.com>
2021-01-11 12:24:22 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
 
-const MATERIALS = [MatButtonModule];
+const MATERIALS = [MatButtonModule, MatInputModule];
 
 @NgModule({
   imports: MATERIALS,
