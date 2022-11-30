402b7b0faf3dc6e9d88ea476975859b33c10a9f9
dymafr <32801166+dymafr@users.noreply.github.com>
2022-11-30 20:00:49 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,18 @@
-import { Component } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public form: FormGroup;
+  public form = this.fb.group({
+    input: [],
+    majeur: [],
+    gender: [],
+    toggle: [],
+  });
 
   constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.createForm();
-  }
-
-  createForm() {
-    this.form = this.fb.group({
-      input: [],
-      majeur: [],
-      gender: [],
-      toggle: []
-    });
-  }
 }
